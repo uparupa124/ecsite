132eb94625ba685b548b246f8a91e13117c86f52
EC2 Default User <ec2-user@ip-172-31-79-246.ec2.internal>
2021-02-09 06:28:55 +0000

    カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,22 +1,12 @@
 class Public::CartItemsController < ApplicationController
-  def index
-    @cart_item = CartItem.all
-  end
 
-  def update
-  end
-
-  def destroy
-  end
-
-  def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.save
-    redirect_to cart_items_path
-  end
-  
+ def create
+   @cart_item = current_customer.cart_items.build(cart_item_params)
+   @cart_item.save
+ end
+ 
   private
    def cart_item_params
-     params.require(:cart_item).permit(:item_id, :amount)
+     params.require(:cart_item).permit(:customer, :item, :amount)
    end
 end
